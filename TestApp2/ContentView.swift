fc7b44997e5af91cd533d76b5539ce4459e153db
codemstrneel <neel@kolhe.org>
2024-08-07 21:25:18 -0700

    committing test app

--- TestApp2/ContentView.swift
+++ TestApp2/ContentView.swift
@@ -1,22 +1,15 @@
-//
-//  ContentView.swift
-//  TestApp2
-//
-//  Created by Neel Kolhe on 8/7/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        WebView(url: URL(string: "https://demo.voicegain.ai/voicebot")!)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
